30d5f50400bb5cec234eaa7e1f593218d1b54559
Youngjun Kwon <Youngjun.Kwon@students.mq.edu.au>
2025-11-05 13:11:33 +0900

    checkout before merge

--- frontend/src/component/ProtectedRoute.jsx
+++ frontend/src/component/ProtectedRoute.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import authService from "../services/auth";
 
-const ProtectedRoute = (requiredRoles) => {
-  const {isAuthenticated, role} = authService.isAuthenticated();
-  
+const ProtectedRoute = ({ requiredRoles }) => {
+  //로그인 상태확인
+  const { isAuthenticated } = authService.isAuthenticated();
+
+  // 현재 사용자 정보
+  const user = authService.getCurrentUser();
+
+  // 사용자 객체에서 role필드 추출
+  const role = user ? user.role : null;
+
+
   console.log(isAuthenticated);
+  console.log(role);
+
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
-  // 2. 권한이 없는 경우: 403 페이지나 메인 페이지로 리다이렉션
+  
+  // 권한이 없는 경우: 메인 페이지로 리다이렉션
   if (requiredRoles && !requiredRoles.includes(role)) {
     alert("접근 권한이 없습니다.");
-    // 권한 없는 경우 메인 페이지 또는 403 에러 페이지로 이동
     return <Navigate to="/" replace />;
   }
   return <Outlet />;
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useAuthStore from "../stores/authStore";
-import { useNavigate } from 'react-router-dom';
 
 function UserPlusIcon(props) {
   return (
